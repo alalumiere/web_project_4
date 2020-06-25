da1c42a15e856a5d3e696c83caa788a7df3d5f9c
Amanda LaLumiere <amandalalumiere@gmail.com>
2020-06-25 12:45:23 -0500

    hotfix: remove modifiers and replace img with buttons

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const editButton = document.querySelector('.profile__info_edit-button');
+const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 const popUp = document.querySelector('.popup');
 const form = document.querySelector('.form');
 const inputName = document.querySelector('.form__name');
 const inputDescription = document.querySelector('.form__description');
-const profileName = document.querySelector('.profile__info_name');
-const profileDescription = document.querySelector('.profile__info_explorer'); 
-const saveButton = document.querySelector('.form__submit-button')
-
-
-
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__explorer');
 
 function togglepopUp() {
   popUp.classList.toggle('popup_open');
+
 }
 
   editButton.addEventListener('click', togglepopUp)
